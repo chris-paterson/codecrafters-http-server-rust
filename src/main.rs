aa1326112479f92d482748a040196955d4de0537
Chris Paterson <cjpaterson.3@gmail.com>
2023-11-04 15:56:35 -0400

    Add support concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-use std::io::{BufRead, BufReader, Write};
-use std::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    listener.set_nonblocking(true);
+    let listener = TcpListener::bind("127.0.0.1:4221").await?;
 
-    for stream in listener.incoming() {
-        handle_connection(stream?);
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(stream).await {
+                println!("Error handling connection: {}", e);
+            }
+        });
     }
-
-    Ok(())
 }
 
 struct Request {
     path: String,
-    user_agent: String,
+    user_agent: Option<String>,
 }
 
 impl<T: AsRef<str>> From<Vec<T>> for Request {
@@ -36,7 +40,11 @@ impl<T: AsRef<str>> From<Vec<T>> for Request {
             .collect();
 
         let user_agent_parts: Vec<_> = user_agent_line.split(" ").collect();
-        let user_agent: String = user_agent_parts[1].into();
+
+        let user_agent: Option<String> = match user_agent_parts.is_empty() {
+            true => None,
+            false => Some(user_agent_parts[1].into()),
+        };
 
         Request { path, user_agent }
     }
@@ -83,24 +91,31 @@ impl Request {
         let first = parts.remove(0);
         return match first {
             "echo" => HttpResponse::Ok(Some(String::from(parts.join("/")))),
-            "user-agent" => HttpResponse::Ok(Some(String::from(self.user_agent))),
+            "user-agent" => HttpResponse::Ok(Some(String::from(self.user_agent.unwrap()))),
             _ => HttpResponse::NotFound,
         };
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let mut lines = buf_reader.lines();
+
+    let mut http_request = Vec::new();
+
+    while let Some(line) = lines.next_line().await? {
+        if line.is_empty() {
+            break;
+        }
+        http_request.push(line);
+    }
 
     println!("Request: {:#?}", http_request);
 
     let request = Request::from(http_request);
     let response = request.handle_route();
 
-    stream.write_all(response.to_string().as_bytes()).unwrap();
+    stream.write_all(response.to_string().as_bytes()).await?;
+
+    Ok(())
 }
