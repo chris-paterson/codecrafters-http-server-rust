d85840ec3f150e1b4c209a074412ab30bf98a650
Chris Paterson <cjpaterson.3@gmail.com>
2023-11-04 19:13:54 -0400

    Add directory path for files

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
+use itertools::Itertools;
+use std::path::PathBuf;
+use std::{env, fs};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
 
+#[derive(Clone, Debug)]
+struct ProgramEnv {
+    files_dir: Option<PathBuf>,
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    let args: Vec<String> = env::args().collect();
+
+    let mut files_dir: Option<PathBuf> = None;
+    for (current, next) in args.into_iter().skip(1).tuples() {
+        if current == "--directory" {
+            files_dir = Some(PathBuf::from(next));
+        }
+    }
+
+    let program_env = ProgramEnv { files_dir };
+
     let listener = TcpListener::bind("127.0.0.1:4221").await?;
 
     loop {
         let (stream, _) = listener.accept().await?;
 
+        let env_clone = program_env.clone();
         tokio::spawn(async move {
-            if let Err(e) = handle_connection(stream).await {
+            if let Err(e) = handle_connection(stream, env_clone).await {
                 println!("Error handling connection: {}", e);
             }
         });
@@ -61,6 +80,8 @@ enum HttpResponse {
 
     /// body
     Ok(Option<String>),
+
+    File(Vec<u8>),
 }
 
 impl HttpResponse {
@@ -78,12 +99,20 @@ impl HttpResponse {
                     return format!("{}\r\n\r\n{}", header, body);
                 }
             },
+            HttpResponse::File(data) => {
+                let header = format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}",
+                        data.len()
+                    );
+                let body = format!("{:?}", data);
+                return format!("{}\r\n\r\n{}", header, body);
+            }
         }
     }
 }
 
 impl Request {
-    fn handle_route(self) -> HttpResponse {
+    fn handle_route(self, env: &ProgramEnv) -> HttpResponse {
         if self.path == "/" {
             return HttpResponse::Ok(None);
         }
@@ -98,12 +127,26 @@ impl Request {
         return match first {
             "echo" => HttpResponse::Ok(Some(String::from(parts.join("/")))),
             "user-agent" => HttpResponse::Ok(Some(String::from(self.user_agent.unwrap()))),
+            "files" => {
+                match env.files_dir.clone() {
+                    None => return HttpResponse::NotFound,
+                    Some(mut files_dir) => {
+                        let filename = parts.join("/");
+                        files_dir.push(&filename);
+
+                        match fs::read_to_string(files_dir) {
+                            Err(_) => return HttpResponse::NotFound,
+                            Ok(content) => return HttpResponse::File(content.as_bytes().into()),
+                        }
+                    }
+                };
+            }
             _ => HttpResponse::NotFound,
         };
     }
 }
 
-async fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+async fn handle_connection(mut stream: TcpStream, env: ProgramEnv) -> std::io::Result<()> {
     let buf_reader = BufReader::new(&mut stream);
     let mut lines = buf_reader.lines();
 
@@ -116,10 +159,10 @@ async fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
         http_request.push(line);
     }
 
-    println!("Request: {:#?}", http_request);
+    println!("Env: {:?}\nRequest: {:#?}", env, http_request);
 
     let request = Request::from(http_request);
-    let response = request.handle_route();
+    let response = request.handle_route(&env);
 
     stream.write_all(response.to_string().as_bytes()).await?;
 
